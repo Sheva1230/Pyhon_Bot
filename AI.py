4bdc17dc5be76f57759e766ced679943edf70cdc
nikita0607 <ecfed205@gmail.com>
2021-07-27 05:41:35 +0800

    Переработка класса AI и main файла, добавление новых AIX классов

--- AI.py
+++ AI.py
@@ -1,51 +1,59 @@
 import re
+import json
+import random
 
 
 class AIMessageTypes:
     """ Типы сообщений """
 
-    UNKNOWN = -1  #
-    START = 0
-    HELLO = 1
-    WHAT_YOU_CAN = 2
+    UNKNOWN = -1
+    HELLO = 0
+    COMMAND = 1
+
+
+class Command:
+    def __init__(self, command, args):
+        self.command: str = command
+        self.args: list = args
+
+
+class AICensure:
+    def __init__(self, answer, topic):
+        self.answer: str = answer
+        self.topic = topic
+
+
+class AIMessage:
+    def __init__(self, type, topic, answer):
+        self.type = type
+        self.topic = topic
+        self.answer: str = answer
+
+
+class AIRecognized:
+    def __init__(self):
+        self.to_me = False
+        self.message: AIMessage = None
+        self.command: Command = None
+        self.topic = None
+        self.censure: AICensure = None
+        self.type = None
 
 
 class AI:
     """  Класс, отвечающий за 'понимание речи' """
 
-    # Фразы приветствия, рассортированные по тематикам
-    generated_hello_phrases = {
-        "NO_TEMA": ["Привет {}!",
-                    "Здравствуй {}, я могучий бот!",
-                    "Просто здравствуй {}, просто как дела..."],
-        "STALKER": ["Приветствую тебя, путник {}!",
-                    "Проходи {}, не задерживайся."],
-        "BOT": ["Привет бип@ {} @бопб@п. С чего ты вз@л, что я ![кожанный мешок]?",
-                "Привет {}. Ты ошибся. Я просто [кожаный мешок]"]
-    }
+    def __init__(self):
+        """ Загрузка всех конфигов """
 
-    # Тематики сообщений и их ключевые слова
-    temas = {
-        "STALKER": [
-            "сталкер",
-            "меченый",
-        ],
-        "BOT": [
-            "бот",
-            "машина",
-            "не человек"
-        ]
-    }
+        with open("answer_phrases.json", encoding="utf-8") as file:
+            self.answer_phrases = json.load(file)
 
-    # Зацензуренные слова
-    censure = {
-        1: ["хуй", "бля", "сука", "чмо", "пид[о]*р"],
-        0: ["писька", "какашка", "писюн"],
-    }
+        with open("key_words.json", encoding="utf-8") as file:
+            self.key_words = json.load(file)
 
-    # Другие ключевые слова
-    start_words = ["старт", "начать"]
-    hello_words = ["привет", "здравствуй", "здарова", "хай", ["добрый", ["день", "вечер"]]]
+        with open("ai_config.json", encoding="utf-8") as file:
+            self.ai_config = json.load(file)
 
     @staticmethod
     def recognite_for_words(message, words) -> bool:
@@ -80,24 +88,76 @@ class AI:
                 return True
         return False
 
-    def recognite(self, message):
+    def get_topic(self, message):
+        """ Получить (возможную) тематику разговора """
+
+        for topic in self.key_words["TOPICS"]:
+            if self.recognite_for_words(message, self.key_words["TOPICS"][topic]):
+                return topic
+
+        return "NO_TOPIC"
+
+    def get_message_type(self, message):
         """ Получить тип сообщения """
-        if self.recognite_for_words(message, self.start_words):
-            return AIMessageTypes.START
-        if self.recognite_for_words(message, self.hello_words):
+
+        if message.startswith(self.ai_config["command_prefix"]):
+            return AIMessageTypes.COMMAND
+
+        if message.count(" ") > 2:
+            _message = message
+            message = message.split()
+            message = message[0] + " " + message[1]
+        print(message)
+
+        if self.recognite_for_words(message, self.key_words["HELLO"]):
+            print("WHAT")
             return AIMessageTypes.HELLO
+
         return AIMessageTypes.UNKNOWN
 
-    def get_tema(self, message):
-        """ Получить тематику сообщения """
-        for tema in self.temas:
-            if self.recognite_for_words(message, self.temas[tema]):
-                return tema
-        return "NO_TEMA"
+    def censure_check(self, message):
+        """ Проверка на цензуру """
 
-    def has_censure(self, message):
-        """ Проверка сообщения на цензуру """
-        for censure_level in self.censure:
-            if self.recognite_for_words(message, self.censure[censure_level]):
-                return censure_level
-        return -1
+        if self.recognite_for_words(message, self.key_words["CENSURE"]):
+            return True
+        return False
+
+    @staticmethod
+    def get_random_answer_by_topic(_from: dict, topic):
+        """ Взять рандомный ответ исходя из темы """
+
+        if topic in _from:
+            return random.choice(_from[topic])
+        return random.choice(_from["NO_TOPIC"])
+
+    def recognite(self, message) -> AIRecognized:
+        ai_message = AIRecognized()   # Объект ответа
+
+        # Проверяем, обратились ли к боту
+        if len(re.findall("\[club*|python bot\]", message)) or message.startswith("куниц"):
+            ai_message.to_me = True
+            message = re.sub("\[club*|python bot\]", message, "").replace("куниц", "")
+
+        message_type = self.get_message_type(message)
+        ai_message.type = message_type
+
+        if message_type == AIMessageTypes.COMMAND:   # Если сообщение это комманда
+            message = message.replace(self.ai_config["command_prefix"], "")
+
+            ai_message.command = Command(message.split()[0], None)
+            if message.count(" "):
+                ai_message.command.args = message.split()[1:]
+        else:
+            topic = self.get_topic(message)
+            ai_message.message = AIMessage(message_type, topic, "")
+
+            if message_type == AIMessageTypes.HELLO:
+                ai_message.message.answer = self.get_random_answer_by_topic(self.answer_phrases["HELLO"], topic)
+            else:
+                ai_message.message.answer = self.get_random_answer_by_topic(self.answer_phrases["UNKNOWN"], topic)
+
+        if self.censure_check(message):
+            topic = self.get_topic(message)
+            ai_message.censure = AICensure(self.get_random_answer_by_topic(self.answer_phrases["CENSURE"], topic), topic)
+
+        return ai_message
--- main.py
+++ main.py
@@ -35,6 +35,9 @@ class VkBot(Bot):   # Класс бота
     def get_warnings(self, chat_id, user_id):   # Получить пред. пользователю через id чата и пользователя
         return self.database.get_field(chat_id, user_id, "warnings", DefaultTypes.INT)
 
+    async def get_user_from_message(self, message: Message):
+        return (await self.api.users.get(message.from_id))[0]
+
 
 if not os.path.isfile("config.json"):
     with open("config.json", 'w') as file:
@@ -50,40 +53,27 @@ ai = AI()
 @bot.on.message()
 async def message(message: Message):
     text = message.text.lower()
+    user = await bot.get_user_from_message(message)
+    ai_recognize = ai.recognite(text)
 
-    has_censure = ai.has_censure(text)   # Проверяем наличие цензуры
-    if has_censure > -1:   # -1 означает, что цензура не обнаружена
-        if has_censure == 0:   # Если полученна цензура не высокого уровня
-            await message.answer("Пожалуйста, постарайтесь воздержаться от таких выражений")
-        elif has_censure == 1:   # Если цензура высокого уровня
-            await message.answer("Вы сказали очень плохое слово!")
-            bot.add_warning_msg(message)
-            await message.answer(f"Это ваше {bot.get_warnings_msg(message)} предупреждение!")
+    if ai_recognize.censure is not None:
+        bot.add_warning_msg(message)
+        await message.answer(ai_recognize.censure.answer.format(user.first_name, user.last_name))
+        await message.answer(f"Это ваше {bot.get_warnings_msg(message)} предупреждение")
+        pass
 
-    recognited = ai.recognite(text)   # Получаем тип сообщения
+    if ai_recognize.type == AIMessageTypes.COMMAND:
+        if ai_recognize.command.command == "test":
+            await message.answer("test passed")
 
-    if recognited == AIMessageTypes.HELLO:   # Если это приветствие
-        name = (await bot.api.users.get(message.from_id))[0].first_name
-        tema = ai.get_tema(text)    # Запрашиваем тематику сообщения
-        if tema in ai.generated_hello_phrases:   # Если такая тематика присутствует в заготовл. приветствиях
-            phrase = ai.generated_hello_phrases[tema][random.randint(0, len(ai.generated_hello_phrases[tema])-1)]
-        else:    # Отрабатываем стандартные приветствия
-            phrase = ai.generated_hello_phrases["NO_TEMA"][random.randint(0, len(ai.generated_hello_phrases["NO_TEMA"]) - 1)]
-        await message.answer(phrase.format(name))
+    if ai_recognize.type == AIMessageTypes.HELLO:
+        await message.answer(ai_recognize.message.answer.format(user.first_name, user.last_name))
 
-    # Проверка на то, было ли это обращение к боту
-    if not len(re.findall("\[club*|python bot\]", text)) and not text.startswith("куниц"):
+    if not ai_recognize.to_me:
         return
 
-    if recognited == AIMessageTypes.UNKNOWN:   # Если не удалось определить тип сообщения
-        tema = ai.get_tema(text)   # Запрашиваем тематику сообщения
-
-        if tema == "STALKER":
-            await message.answer("Не знаю фраер, шо ты вякнул, но я не в обиде")
-        elif tema == "BOT":
-            await message.answer("#@%#1337$ERROR: Не могу ответить на ваш запрос#52%(^&$@^(%228")
-        else:
-            await message.answer("Я не понимаю о чем вы говорите!")
+    if ai_recognize.type == AIMessageTypes.UNKNOWN:
+        await message.answer(ai_recognize.message.answer)
 
 
 print("!Run!")
